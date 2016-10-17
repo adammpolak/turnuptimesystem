561e1bf4ecc69f250ac8330bdbf56fe3be305b89
Rachel Beneroff <rbbeneroff@gmail.com>
2016-10-17 18:43:23 -0400

    addProject function

--- public/js/projectsController.js
+++ public/js/projectsController.js
@@ -10,17 +10,28 @@
     this.projects = [];
     this.completedprojects = [];
 
+//get the project data
+    $http.get('/projects')
+    .then(function(response) {
+      self.projects = response.data;
+    })
+
 // ADD PROJECT FUNCTION
     var addProject = function(project) {
       console.log('clicked add project button')
       this.projects.push(project);
       console.log(this.projects, 'projects added');
-      $http.post('/')
-
+      $http.post('/projects', this.projects)
+      .then(function(response) {
+        $location.path('/projects')
+      })
+      .catch(function(err) {
+        console.log(err)
+      });
     }
 
 
-
+    this.addProject = addToCart;
 
   }
 })()
