9b4ef192aacb5b4f582a8cd87722e46d93d06cc3
Rachel Beneroff <rbbeneroff@gmail.com>
2016-10-17 18:25:22 -0400

    working in public/js/projectsController

--- public/js/projectsController.js
+++ public/js/projectsController.js
@@ -2,7 +2,25 @@
   angular.module('turnupApp')
   .controller('projectsController', projectsController);
   projectsController.$inject = ['$http', '$location'];
+
   function projectsController($http, $location) {
+    var self = this;
     this.number = 7;
+
+    this.projects = [];
+    this.completedprojects = [];
+
+// ADD PROJECT FUNCTION
+    var addProject = function(project) {
+      console.log('clicked add project button')
+      this.projects.push(project);
+      console.log(this.projects, 'projects added');
+      $http.post('/')
+
+    }
+
+
+
+
   }
 })()
