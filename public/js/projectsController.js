f0cd8d067b5b9132c3c407ce46b5cf4804ff4e19
adammpolak <adam.polak@citrusbyte.com>
2016-10-17 17:39:36 -0700

    Controller fixes

--- public/js/projectsController.js
+++ public/js/projectsController.js
@@ -9,6 +9,9 @@
 
     this.projects = [];
     this.completedprojects = [];
+    this.newProjectTasks = [{name: '', description: ''}]
+
+//add new options for create new project
 
 //get the project data for projects page
     $http.get('/projects')
@@ -25,10 +28,10 @@
 // ADD PROJECT FUNCTION
     var addProject = function(project) {
       console.log('clicked add project button')
-      this.projects.push(project);
       console.log(this.projects, 'projects added');
       $http.post('/projects', this.projects)
       .then(function(response) {
+        self.projects.push(project);
         $location.path('/projects')
       })
       .catch(function(err) {
@@ -38,10 +41,10 @@
 
 // EDIT/UPDATE PROJECT FUNCTION
     var editProject = function(project) {
-      this.projects.push(project);
       console.log(this.projects, 'has been updated');
-      $http.post(`/projects/${id}`) //want to post to projects/:id, correct?
+      $http.post(`/projects/${project._id}`,) //want to post to projects/:id, correct?
       .then(function(response) {
+        self.projects.push(response.data);
         $location.path('/projects/project')
       })
       .catch(function(err) {
