edc3b6f86015dcc93ae68129c3b60ddd4fe64aea
adammpolak <adam.polak@citrusbyte.com>
2016-10-19 01:34:58 -0700

    Add edit and delete functionalities

--- public/js/projectsController.js
+++ public/js/projectsController.js
@@ -11,6 +11,10 @@
     this.completedprojects = [];
     this.activeProject = {};
     this.activeUserTaskStates = [];
+    this.removeTask = function(index) {
+      self.activeProject.taskList[index].splice(index, 1);
+      self.updateActiveProject();
+    }
 //update time for a certain task
     this.updateTaskTimePeriod = function(index){
       var now = new Date();
@@ -36,8 +40,6 @@
           outstanding: true,
           indexPosition: self.activeProject.taskList[index].taskTimeList.length-1,
         }
-        console.log(self.activeProject);
-        console.log(self.activeUserTaskStates);
       }
       //this will be the put route to update the new object with the information
       self.updateActiveProject();
@@ -73,7 +75,6 @@
         for (var i = 0; i<self.activeProject.taskList[x].taskTimeList.length; i++) {
 
           if (self.activeProject.taskList[x].taskTimeList[i].stop) {
-            console.log('it exists!');
           } else {
             if (self.activeProject.taskList[x].taskTimeList[i].userId == self.currentUser._id) {
               taskStatus = {
@@ -82,11 +83,9 @@
               }
             }
           }
-          console.log(self.activeProject.taskList[x].taskTimeList[i]);
         }
         self.activeUserTaskStates.push(taskStatus);
       }
-      console.log(self.activeUserTaskStates);
     }
 
 //when trying to create a new project this is what you need
@@ -103,6 +102,11 @@
     .then(function(response) {
       self.allProjects = response.data;
     })
+    // $http.get($location.$$path.toString())
+    // .then(function(response) {
+    //   self.activeProject = response.data;
+    // })
+    // console.log($location.$$path.toString())
 
 // get completed projects for completed projets page
     // $http.get('/api/completedprojects')
@@ -111,6 +115,17 @@
     // })
 
 // ADD PROJECT FUNCTION
+    this.getToProject = function(){
+      $location.path('/projects/project');
+    }
+    this.getToProjects = function(){
+      $location.path('/projects');
+      $http.get('/api/projects')
+      .then(function(response) {
+        self.allProjects = response.data;
+      })
+    }
+
     var addProject = function(project) {
       project.taskList = self.newProjectTasks;
       console.log('clicked add project button')
@@ -130,25 +145,16 @@
         console.log(err)
       });
     }
+//REMOVE PROEJCT FUNCTION
+    this.deleteProject = function () {
+      $http.delete(`/api/projects/project/${self.activeProject._id}`)
+      .then(function(response){
+        self.activeProject = {};
 
-// EDIT/UPDATE PROJECT FUNCTION
-    var editProject = function(project) {
-      console.log(this.allProjects, 'has been updated');
-      $http.post(`/projects/${project._id}`) //want to post to projects/:id, correct?
-      .then(function(response) {
-        self.projects.push(response.data);
-        $location.path('/projects/project')
       })
-      .catch(function(err) {
-        console.log(err)
-      });
     }
 
-
-
     this.addProject = addProject;
-    this.editProject = editProject;
-
   }
 
 })()
