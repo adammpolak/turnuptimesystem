805b2707a30caab5244c12b6d2ef7ea65d105869
Rachel Beneroff <rbbeneroff@gmail.com>
2016-10-19 12:41:18 -0400

    completed project view working with dot notation

--- public/js/projectsController.js
+++ public/js/projectsController.js
@@ -22,67 +22,23 @@
 
 //updating a project status
     this.updateStatus = function () {
-    // allProjects.$save(function(updateStatus) {
       console.log(self.activeProject.completed);
       self.activeProject.completed = !self.activeProject.completed
       console.log('update function', self.activeProject.completed);
-
-      self.completedprojects.push(self.activeProject);
-      console.log(self.completedprojects, "completed projects");
-      var completedIndex = self.allProjects.indexOf(this.activeProject);
-      self.allProjects.splice(completedIndex, 1);
-
-      $http.patch(`/api/projects/${this.activeProject._id}`, this.activeProject)
-      .then(function(response) {
-        console.log(this.activeProject);
-      })
+      //NEED BUT PATCH FUNCTION NOT COMPLETED YET
+      // $http.patch(`/api/projects/${this.activeProject._id}`, this.activeProject)
+      // .then(function(response) {
+      //   console.log(this.activeProject);
+      // })
       console.log(self.allProjects, 'updated without completed');
 
-      // return $resource('/api/projects', {},
-      //   { save: {
-      //       method: 'POST',
-      //       isArray: true
-      //     }});
-      // self.allProjects.completedIndex = self.allProjects.$save();
-      // this.activeProject = project;
-      // $http.patch(`/api/projects/${project._id}`, self.activeProject)
-      // .then(function(response) {
-      //   self.activeProject.completed = '';
-      // })
-      // self.completedprojects.save();
-      // $http.put('/api/projects')
-      // .then(function(response) {
-      //   self.allProjects.$save();
-      //   // self.allProjects.push(response.data);
-      // })
-      // .catch(function(err) {
-      //   console.log(err)
-      // });
+      //not needed
+      // self.completedprojects.push(self.activeProject);
+      // console.log(self.completedprojects, "completed projects");
+      // var completedIndex = self.allProjects.indexOf(this.activeProject);
+      // self.allProjects.splice(completedIndex, 1);
 
-
-      // $location.path('/projects');
-      // $http.put(`/api/projects/${self.activeProject._id}`, self.activeProject)
-      // .then(function(response){
-      //   // $state.go('completedprojects', {url: '/completedprojects'})
-      // })
     }
-
-  // display completed projects
-  // this.removeCompletedFromAll = function() {
-  //   $http.post('/api/projects', send)
-  //   .then(function(response) {
-  //     allProjects.save(function(err, allProjects) {
-  //       console.log(err);
-  //       // $location.path('/projects')
-  //     })
-  //     // self.allProjects.push(response.data);
-  //   })
-  //   .catch(function(err) {
-  //     console.log(err)
-  //   });
-  //   console.log(self.allprojects, 'updated without completed');
-  //
-  // }
 
 //display a project
     this.displayThisProject = function(index) {
