a0fe66b3aa2b908c2319ecdbba4c0c9d2546a54f
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-19 14:52:06 -0500

    More robust flash, redid login to accomodate passport sending json

--- public/js/usersController.js
+++ public/js/usersController.js
@@ -9,33 +9,24 @@
     function register(userObj){
       $http.post('api/users/register', {username: userObj.usernamereg, password: userObj.passwordreg})
         .then(function(res){
-<<<<<<< Updated upstream
-          $state.go('projects', {url: '/projects'});
-          successAlert();
-          userObj.passwordreg = '';
-          userObj.usernamereg = '';
-=======
-          if (res.errmsg){
+          if (res.data.message){
             $state.go('landing', {url: '/'});
-            authFail(res.errmsg.toString());
+            authFail(res.data.message);
           } else {
             $state.go('projects', {url: '/projects'});
             successAlert();
             userObj.passwordreg = '';
             userObj.usernamereg = '';
           }
->>>>>>> Stashed changes
         })
     }
 
     function login(userObj){
       $http.post('api/users/login', {username: userObj.username, password: userObj.password})
         .then(function(res){
-          console.log(res);
-          console.log(res.errmsg);
-          if (res.errmsg) {
+          if (res.data.message) {
             $state.go('landing', {url: '/'});
-            authFail(res.errmsg.toString());
+            authFail(res.data.message);
           } else {
             self.user = res.data.user;
             console.log(self.user)
@@ -56,16 +47,12 @@
     }
 
     function authFail(reason){
-      var message = '<strong>Failure!</strong>' + reason;
+      var message = reason;
       var id = Flash.create('danger', message, 7000, {}, true);
     }
 
     this.login = login;
     this.register = register;
-<<<<<<< Updated upstream
-    // this.successAlert = successAlert;
-=======
->>>>>>> Stashed changes
     this.userObject;
   };
 })();
